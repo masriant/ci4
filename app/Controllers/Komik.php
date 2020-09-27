148cb21c834e960a91e057988a3bc8701e733304
masriant <info.masrianto@gmail.com>
2020-09-27 22:36:40 +0700

    koneksi ke db dengan model

--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\KomikModel;
+
 class Komik extends BaseController
 {
   public function index()
@@ -9,12 +11,14 @@ class Komik extends BaseController
     $data = [
       'title' => 'Daftar Komik'
     ];
-    // cara konneksi db tanpa model
-    $db = \Config\Database::connect();
-    $komik = $db->query("SELECT * FROM komik");
-    foreach ($komik->getResultArray() as $row) {
-      d($row);
-    }
+
+
+    // $komikModel = new \App\Models\KomikModel();
+
+    $komikModel = new KomikModel();
+    $komik = $komikModel->findAll();
+    dd($komik);
+
     return view('komik/index', $data);
   }
   //--------------------------------------------------------------------
