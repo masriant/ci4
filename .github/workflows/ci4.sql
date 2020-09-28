dad34c22eaeafef6abb4e8790526e0c761ad0343
masriant <info.masrianto@gmail.com>
2020-09-29 06:36:05 +0700

    form untuk file gambar

---
+++ .github/workflows/ci4.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Waktu pembuatan: 28 Sep 2020 pada 22.35
+-- Versi server: 10.4.10-MariaDB
+-- Versi PHP: 7.4.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ci4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `komik`
+--
+
+DROP TABLE IF EXISTS `komik`;
+CREATE TABLE IF NOT EXISTS `komik` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `judul` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `penulis` varchar(255) NOT NULL,
+  `penerbit` varchar(255) NOT NULL,
+  `sampul` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- app/Views/komik/create.php
+++ app/Views/komik/create.php
@@ -5,11 +5,7 @@
   <div class="row">
     <div class="col-8">
       <h2 class="mt-3">Form Tambah Data</h2>
-      <!-- validation -->
-      <!-- <//?= $validation->listErrors(); ?> -->
-      <!-- endvalidation -->
-      <!-- Form -->
-      <form action="/komik/save" method="POST">
+      <form action="/komik/save" method="POST" enctype="multipart/form-data">
         <?= csrf_field(); ?>
         <div class="row mb-3">
           <label for="judul" class="col-sm-2 col-form-label">Judul</label>
@@ -44,14 +40,36 @@
         <div class="row mb-3">
           <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
           <div class="col-sm-8">
-            <input type="text" class="form-control <?= ($validation->hasError('sampul')) ? 'is-invalid' : ''; ?>"
-              id="sampul" name="sampul" value="<?= old('sampul'); ?>">
+            <!-- <input type="text" class="form-control <//?= ($validation->hasError('sampul')) ? 'is-invalid' : ''; ?>"
+              id="sampul" name="sampul" value="<//?= old('sampul'); ?>"> -->
+
+            <!-- v5 -->
+            <div class="form-file form-file-sm">
+              <input type="file" class="form-file-input" id="sampul">
+              <label class="form-file-label" for="Sampul">
+                <span class="form-file-text">Pilih gambar...</span>
+                <span class="form-file-button">Browse</span>
+              </label>
+            </div>
+            <!-- v5 -->
+
+            <!-- v4 -->
+            <!-- <div class="custom-file">
+              <input type="file" class="custom-file-input" id="customFile">
+              <label class="custom-file-label" for="customFile">Choose File</label>
+            </div> -->
+            <!-- v4 -->
+
             <div class="invalid-feedback">
               <?= $validation->getError('sampul'); ?>
             </div>
           </div>
         </div>
-        <button type="submit" class="btn btn-primary">Tambah Data</button>
+        <div class="row mb-3">
+          <div class="col-sm-8">
+            <button type="submit" class="btn btn-primary">Tambah Data</button>
+          </div>
+        </div>
 
         <!--  -->
       </form>
