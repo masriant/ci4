e560e28a9b66dc52b36909525457da9fc2e10389
masriant <info.masrianto@gmail.com>
2020-09-29 13:13:39 +0700

    Simple dan Query Builder insert & insertBatch

--- app/Database/Seeds/OrangSeeder.php
+++ app/Database/Seeds/OrangSeeder.php
@@ -8,7 +8,36 @@ class OrangSeeder extends \CodeIgniter\Database\Seeder
 {
   public function run()
   {
-    // Test 4 OrangSeeder Query Builder => php spark db:seed OrangSeeder
+
+    // Test 2 ok OrangSeeder Simple Queries => php spark db:seed OrangSeeder
+    // $data = [
+    //   'nama'        => 'Masrianto',
+    //   'alamat'      => 'Jakarta',
+    //   'created_at'  => Time::now(),
+    //   'updated_at'  => Time::now()
+    // ];
+    // Simple Queries
+    // $this->db->query(
+    //   "INSERT INTO orang (nama, alamat, created_at, updated_at) VALUES(:nama:, :alamat:, :created_at:, :updated_at:)",
+    //   $data
+    // );
+
+
+    //---------------------------------------------------------------------------------
+    // Test 3 Ok OrangSeeder Query Builder => php spark db:seed OrangSeeder
+    // Using Query Builder
+    // $data = [
+    //   'nama'        => 'Masrianto',
+    //   'alamat'      => 'Jakarta',
+    //   'created_at'  => Time::now(),
+    //   'updated_at'  => Time::now()
+    // ];
+    // Using Query Builder
+    // $this->db->table('orang')->insert($data);
+    //---------------------------------------------------------------------------------
+
+
+    // Test 4 ok OrangSeeder Query Builder insertBatch => php spark db:seed OrangSeeder
     // array didalam array
     $data = [
       [
@@ -36,7 +65,6 @@ class OrangSeeder extends \CodeIgniter\Database\Seeder
         'updated_at'  => Time::now()
       ]
     ];
-
 
     // Using Query Builder
     // $this->db->table('orang')->insert($data);
